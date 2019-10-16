326823646e1498017d9b3fbb3d7ee33141a5310a
jansen <z5141970@student.unsw.edu.au>
2019-10-16 12:12:11 +1100

    added some form validation

--- static/src/js/signup.js
+++ static/src/js/signup.js
@@ -1,34 +1,23 @@
 $(document).ready(function() {
-    console.log( "ready!" );
-    
-    $('#register').click(function (event) {
+ 
+    $('#register').click(function () {
         var name = $('#name').val();
         var email = $('#email').val();
 
         var pass = $('#password').val();
         var repass = $('#repassword').val();
-    
-
+        
         if(name.length == 0 
             || email.length == 0 
             || pass == 0 
             || repass  == 0) {
-            $('#notif').removeClass('hidden');
-            $('#register').addClass('disabled');
-            return false;
-        } else {
-            $('#notif').addClass('hidden');
-            $('#register').removeClass('disabled');
+            alert('Please fill out empty fields');
         }
-
         if(pass !== repass && pass.length > 0 && repass.length > 0) {
-            $('#notif1').removeClass('hidden');
-            $('#register').addClass('disabled');
-            return false;
-        } else {
-            $('#notif1').addClass('hidden');
-            $('#register').removeClass('disabled');
+
+            alert('Passwords do not match');
         }
-        return true;
-    });    
+    });
+
+
 });
