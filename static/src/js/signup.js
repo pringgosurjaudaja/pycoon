fd82a9d157b4ef67369c77c2ac1ea148d58c5256
jansen <z5141970@student.unsw.edu.au>
2019-10-16 12:39:33 +1100

    fixed the signup form

--- static/src/js/signup.js
+++ static/src/js/signup.js
@@ -1,23 +1,69 @@
 $(document).ready(function() {
  
-    $('#register').click(function () {
-        var name = $('#name').val();
-        var email = $('#email').val();
+    // $('#register').click(function () {
+    //     var name = $('#name').val();
+    //     var email = $('#email').val();
 
-        var pass = $('#password').val();
-        var repass = $('#repassword').val();
+    //     var pass = $('#password').val();
+    //     var repass = $('#repassword').val();
         
-        if(name.length == 0 
-            || email.length == 0 
-            || pass == 0 
-            || repass  == 0) {
-            alert('Please fill out empty fields');
-        }
-        if(pass !== repass && pass.length > 0 && repass.length > 0) {
+    //     if(name.length == 0 
+    //         || email.length == 0 
+    //         || pass == 0 
+    //         || repass  == 0) {
+    //         alert('Please fill out empty fields');
+    //     }
+    //     if(pass !== repass && pass.length > 0 && repass.length > 0) {
 
-            alert('Passwords do not match');
-        }
-    });
+    //         alert('Passwords do not match');
+    //     }
+    // });
 
+    $('.form')
+    .form({
+        fields: {
+          name: {
+            identifier: 'name',
+            rules: [
+              {
+                type   : 'empty',
+                prompt : 'Please enter your name'
+              }
+            ]
+          },
+          skills: {
+            identifier: 'email',
+            rules: [
+              {
+                type   : 'empty',
+                prompt : 'Please enter your email'
+              }
+            ]
+          },
+          
+          password: {
+            identifier: 'password',
+            rules: [
+              {
+                type   : 'empty',
+                prompt : 'Please enter a password'
+              }
+            ]
+          },
+          repassword: {
+            identifier: 'repassword',
+            rules: [
+              {
+                type   : 'empty',
+                prompt : 'Please re-confirm password'
+              },
+              {
+                type   : 'match[password]',
+                prompt : 'Password must match'
+              }
+            ]
+          }
+        }
+      })
 
 });
